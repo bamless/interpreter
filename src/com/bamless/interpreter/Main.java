c964c18132dc4d4de382ca04e63bfd7c484f644a
bamless <bamlessnty5@gmail.com>
2017-08-26 19:17:55 +0200

    Remove debug code

--- src/com/bamless/interpreter/Main.java
+++ src/com/bamless/interpreter/Main.java
@@ -2,8 +2,6 @@ package com.bamless.interpreter;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 import com.bamless.interpreter.parser.ASTParser;
 import com.bamless.interpreter.parser.ast.ASTNode;
@@ -12,17 +10,7 @@ import com.bamless.interpreter.parser.ast.visitor.PrinterVisitor;
 public class Main {
 	private final static String LEX_FILE = "/lexical-spec.lex";
 	
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
-
-        ClassLoader cl = ClassLoader.getSystemClassLoader();
-
-        URL[] urls = ((URLClassLoader)cl).getURLs();
-
-        for(URL url: urls){
-        	System.out.println(url.getFile());
-        }
-		
+	public static void main(String[] args) throws FileNotFoundException, IOException {		
 		ASTParser p = new ASTParser();
 		//trick program to show else binding. The x at the end will be 0.
 		ASTNode root = p.parse(ClassLoader.class.getResourceAsStream("/test.lang"));
